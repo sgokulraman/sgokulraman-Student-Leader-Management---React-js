d4192b132d6f9a37523efbbf43a4df9479b9aace
gokul raman <sgokulraman5@gmail.com>
2026-01-23 01:58:32 +0530

    adding new path

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-
 import Mainpage from './Management/Mainpage';
-
 function App() {
 
   return (
